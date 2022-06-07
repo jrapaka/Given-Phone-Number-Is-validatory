a826d12fd7504c83d7c61f94eae94fcdb06ebe10
jrapaka <107037438+jrapaka@users.noreply.github.com>
2022-06-07 16:23:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,27 +24,4 @@ if(valid_code==True):
     else:
 	    print("Invalid Mobile Number")
 
-    import re
- 
-def check_url(ip_url):
-# Regular expression for URL
-    re_exp = ("((http|https)://)(www.)?" + "[a-zA-Z0-9@:%._\\+~#?&//=]" +
-             "{2,256}\\.[a-z]" + "{2,6}\\b([-a-zA-Z0-9@:%" + "._\\+~#?&//=]*)")
-    exp = re.compile(re_exp)
-    if (ip_url == None):
-        print("Input string is empty")
-    if(re.search(exp, ip_url)):
-        print("Input URL is valid!")
-    else:
-        print("Input URL is invalid!")
-ip_url = input("Enter the string: ")
-check_url(ip_url)
-
-import re
-pattern = "^.(?=.{8,})(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[@#$%^&+=]).$"
-password = input("Enter password : ")
-result = re.findall(pattern, password)
-if (result):
-    print ("Valid password")
-else:
-    print ("Password not valid")
+    
